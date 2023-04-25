edea11d74ae431d71fa680508f49ba39bbfc24df
DhanushChinnaiyan <dhanushmd4021@gmail.com>
2023-04-25 18:21:51 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Forgot from "./Components/Forgot";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path="/">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/forgot">
+          <Forgot />
+        </Route>
+      </Switch>
     </div>
   );
 }
